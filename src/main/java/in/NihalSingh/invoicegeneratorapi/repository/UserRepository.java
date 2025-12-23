6aa91f97f750e0e1c03b06d6fa5438aeff8035f7
NIHAL SINGH <nihalsingh2950@gmail.com>
2025-12-23 13:00:06 +0000

    initial commit

---
+++ src/main/java/in/NihalSingh/invoicegeneratorapi/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package in.NihalSingh.invoicegeneratorapi.repository;
+
+import in.NihalSingh.invoicegeneratorapi.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByClerkId(String clerkId);
+
+    boolean existsByClerkId(String clerkId);
+}
---
+++ src/main/java/in/NihalSingh/invoicegeneratorapi/service/InvoiceService.java
@@ -0,0 +1,30 @@
+package in.NihalSingh.invoicegeneratorapi.service;
+
+import in.NihalSingh.invoicegeneratorapi.entity.Invoice;
+import in.NihalSingh.invoicegeneratorapi.repository.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InvoiceService {
+
+    private final InvoiceRepository repository;
+
+    public Invoice saveInvoice(Invoice invoice) {
+        return repository.save(invoice);
+    }
+
+    public List<Invoice> fetchInvoices(String clerkId) {
+        return repository.findByClerkId(clerkId);
+    }
+
+    public void removeInvoice(String clerkId, String invoiceId) {
+        Invoice existingInvoice = repository.findByClerkIdAndId(clerkId, invoiceId)
+                .orElseThrow(() -> new RuntimeException("Invoice not found:" + invoiceId));
+        repository.delete(existingInvoice);
+    }
+}
+
